da90f9a953225ef1774b925567471bcf0b0e8781
dangertater <106702944+dangertater@users.noreply.github.com>
2022-09-21 17:58:03 -0600

    i think this is a callback

--- numberMucker.js
+++ numberMucker.js
@@ -1,6 +1,18 @@
-let numberMucker = () => {
-	let num1 = Math.floor(Math.random() * 11)
-	let num2 = Math.floor(Math.random() * 11)
-	return num1 * num2
+// let numberMucker = () => {
+// 	let num1 = Math.floor(Math.random() * 11)
+// 	let num2 = Math.floor(Math.random() * 11)
+// 	return num1 * num2
+// }
+// export default numberMucker
+
+let numbers = () => {
+	let num1 = 1
+	let num4 = 3
+	return adder(num1, num4)
 }
-export default numberMucker
+
+let adder = (num1, num2) => {
+	return num1 + num2
+}
+
+console.log(numbers())
