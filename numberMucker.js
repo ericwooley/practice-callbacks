41995c09cd560cf56826af2af02f8e3a93fbee09
dangertater <106702944+dangertater@users.noreply.github.com>
2022-09-21 18:17:20 -0600

    test

--- numberMucker.js
+++ numberMucker.js
@@ -5,14 +5,26 @@
 // }
 // export default numberMucker
 
-let numbers = () => {
+// let numbers = (asdf) => {
+// 	let num1 = 1
+// 	let num4 = 3
+// 	return asdf(num1, num4)
+// }
+
+// let adder = (num1, num2) => {
+// 	return num1 + num2
+// }
+
+// console.log(numbers(adder))
+
+let numx = (asdf) => {
 	let num1 = 1
-	let num4 = 3
-	return adder(num1, num4)
+	let num2 = 2
+	return asdf(num1, num2)
 }
 
-let adder = (num1, num2) => {
-	return num1 + num2
+let subtractor = (num1, num2) => {
+	return num2 - num1
 }
 
-console.log(numbers())
+console.log(numx(subtractor))
